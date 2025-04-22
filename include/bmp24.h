8852c96abcfa84685bf9be424c2aed4c3f605b93
Adn1n <adnanmoubarac@icloud.com>
2025-04-22 18:30:27 +0200

    Commencement de la deuxieme partie

--- include/bmp24.h
+++ include/bmp24.h
@@ -15,6 +15,19 @@
 #define BITMAP_WIDTH 0x12 // offset 18
 #define BITMAP_HEIGHT 0x16 // offset 22
 #define BITMAP_DEPTH 0x1C // offset 28
+#define BITMAP_SIZE_RAW 0x22 // offset 34
+
+
+// Constante pour le type de fichier BMP
+#define BMP_TYPE 0x4D42 //'BM' en hexadécimal
+
+// Constantes pour les tailles des champs de l'en-tête BMP
+#define HEADER_SIZE 0x0E // 14 octets
+#define INFO_SIZE 0x28 // 40 octets
+
+// Constantes pour les valeurs de profondeur de couleur
+#define DEFAULT_DEPTH 0x18 // 24
+
 
 
 // Définitions des structures//
@@ -58,6 +71,14 @@ typedef struct {
 
 
 // Prototypes des Fonctions //
+
+t_pixel ** bmp24_allocateDataPixels (int width, int height);
+
+void bmp24_freeDataPixels (t_pixel ** pixels, int height);
+
+t_bmp24 * bmp24_allocate (int width, int height, int colorDepth);
+
+void bmp24_free (t_bmp24 * img);
 
 
 #endif //BMP24_H
--- src/bmp24.c
+++ src/bmp24.c
@@ -3,3 +3,61 @@
 //
 
 #include "bmp24.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+t_pixel ** bmp24_allocateDataPixels (int width, int height) {
+
+    t_pixel ** pixels = malloc(sizeof(t_pixel *) * height);
+    if (pixels == NULL) {
+        printf("Erreur : allocation hauteur\n");
+        return NULL;
+    }
+
+    for (int i = 0; i < height; i++) {
+        pixels[i] = malloc(sizeof(t_pixel) * width);
+        if (pixels[i] == NULL) {
+            printf("Erreur: allocation longueur\n");
+        }
+    }
+    return pixels;
+}
+
+void bmp24_freeDataPixels (t_pixel ** pixels, int height) {
+    for (int i = 0; i < height; i++) {
+        free(pixels[i]);
+    }
+    free(pixels);
+}
+
+t_bmp24 * bmp24_allocate (int width, int height, int colorDepth) {
+
+    t_bmp24 * bmp24 = malloc(sizeof(t_bmp24));
+    if (bmp24 == NULL) {
+        printf("Erreur : allocation hauteur\n");
+        return NULL;
+    }
+
+    bmp24 ->data = bmp24_allocateDataPixels(width, height);
+    if (bmp24->data == NULL) {
+        free(bmp24);
+        printf("Erreur : allocation des pixels échouée\n");
+        return NULL;
+    }
+    bmp24->width = width;
+    bmp24->height = height;
+    bmp24->colorDepth = colorDepth;
+
+    return bmp24;
+
+}
+
+void bmp24_free (t_bmp24 * bmp24) {
+    if (bmp24 != NULL) {
+        bmp24_freeDataPixels(bmp24->data, bmp24->height);
+        free(bmp24);
+    }
+}
+
+
